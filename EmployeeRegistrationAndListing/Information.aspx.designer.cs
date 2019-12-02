bc6a58a69d46fecc8d0d3a1a0a426e0cf8db911d
ianpblc <publicoian@gmail.com>
2019-12-02 14:22:19 +0800

    NavBar

--- EmployeeRegistrationAndListing/Information.aspx.designer.cs
+++ EmployeeRegistrationAndListing/Information.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace EmployeeRegistrationAndListing
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Image2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+
+        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
